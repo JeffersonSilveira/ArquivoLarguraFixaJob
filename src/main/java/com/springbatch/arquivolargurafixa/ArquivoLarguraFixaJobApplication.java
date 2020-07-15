c418197422a83c4eaabd04f40f2ea39d33afc7f3
JeffersonSilveira <jeffersonsilveira30@gmail.com>
2020-07-15 20:08:20 -0300

    Prática: Arquivo Flat, Leitura de arquivo de largura fixa

---
+++ src/main/java/com/springbatch/arquivolargurafixa/ArquivoLarguraFixaJobApplication.java
@@ -0,0 +1,12 @@
+package com.springbatch.arquivolargurafixa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquivoLarguraFixaJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquivoLarguraFixaJobApplication.class, args);
+	}
+}
